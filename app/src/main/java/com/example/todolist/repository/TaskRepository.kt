891aecbdae849d0b16f2180a0482f86ac3533bce
Lazar <sljivic.lazar037@gmail.com>
2022-07-12 10:06:05 +0200

    lifecycle view model, task dao update, task repo

---
+++ app/src/main/java/com/example/todolist/repository/TaskRepository.kt
@@ -0,0 +1,19 @@
+package com.example.todolist.repository
+
+import androidx.lifecycle.LiveData
+import com.example.todolist.database.TaskDao
+import com.example.todolist.database.TaskEntry
+
+class TaskRepository(val taskDao: TaskDao) {
+    suspend fun insert(taskEntry: TaskEntry) = taskDao.insert(taskEntry)
+
+    suspend fun updateData(taskEntry: TaskEntry) = taskDao.update(taskEntry)
+
+    suspend fun deleteItem(taskEntry: TaskEntry) = taskDao.delete(taskEntry)
+
+    suspend fun deleteAll() = taskDao.deleteAll()
+
+    fun getAlTasks(): LiveData<List<TaskEntry>> = taskDao.getAllTasks()
+
+    fun getAllPriorityTasks(): LiveData<List<TaskEntry>> = taskDao.getAllPriorityTasks()
+}
