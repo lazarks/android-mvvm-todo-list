df12c3fea4979d4201a31e767d202a3f1ef7a02d
Lazar <sljivic.lazar037@gmail.com>
2022-07-12 09:57:25 +0200

    database: TaskDatabase + TaskDao

---
+++ app/src/main/java/com/example/todolist/database/TaskDao.kt
@@ -0,0 +1,22 @@
+package com.example.todolist.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface TaskDao {
+    @Insert
+    suspend fun insert(taskEntry: TaskEntry)
+
+    @Delete
+    fun delete(taskEntry: TaskEntry)
+
+    @Update
+    fun update(taskEntry: TaskEntry)
+
+    @Query("DELETE FROM task_table")
+    fun deleteAll()
+
+    @Query("SELECT * FROM task_table ORDER BY timestamp DESC")
+    fun getAllTasks(): LiveData<List<TaskEntry>>
+}
